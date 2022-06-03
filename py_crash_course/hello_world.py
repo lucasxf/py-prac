8f734cddbae575b9582f7997a7d2986faaefda17
lucasxf <lucasxferreira@gmail.com>
2022-06-03 10:58:28 -0300

    #time 30m hello world & project setup

---
+++ py_crash_course/hello_world.py
@@ -0,0 +1,12 @@
+from re import M
+
+
+mensagem = "Oi, mundão! ;)"
+print("Msg:" + mensagem)
+maiusculas = mensagem.capitalize()
+print("Msg: " + mensagem)
+print("Maiúsculas: " +maiusculas)
+#print(palavras) # name 'palavras' is not defined
+palavras = mensagem.split()
+print("Msg: " + mensagem)
+print(palavras) # ['Oi,', 'mundão!', ';)']
